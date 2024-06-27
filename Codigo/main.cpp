9bcb9ee352ca8f24e71474e47673f1a8f9a92eb5
chicho377 <morafabricio86@gmail.com>
2024-06-27 16:35:50 -0600

    Agregando funcion para agregar empleados

--- Codigo/main.cpp
+++ Codigo/main.cpp
@@ -26,7 +26,9 @@ int numEmpleados = 0;
 int cantOperarios = 0, cantTecnicos = 0, cantProfesionales = 0;
 double acumuladoNetoOperarios = 0, acumuladoNetoTecnicos = 0, acumuladoNetoProfesionales = 0;
 
-// inicializando las funciones para poder agregarlas al main
+// funciones
+
+// funcion mostrar menu
 void mostrarMenu() {
     cout << "Menu Principal:\n";
     cout << "1. Agregar empleado\n";
@@ -36,6 +38,37 @@ void mostrarMenu() {
     cout << "5. Mostrar estadisticas finales\n";
     cout << "6. Salir\n";
     cout << "Ingrese una opcion: ";
+}
+
+// funcion agregar empleado
+void agregarEmpleado(){
+	
+	// verificacion de numero maximo de empleados
+	if(numEmpleado >= MAX_EMPLEADOS){
+		cout << "Se ha alcanzado el numero maximo de empleados.\n";
+		return;
+	}
+	
+	// solicitar los datos
+	cout << "Ingrese la cedula del empleado: ";
+    cin >> cedulas[numEmpleados];
+
+    cout << "Ingrese el nombre del empleado: ";
+    cin.ignore(); // consumir el salto de linea pendiente 
+    getline(cin, nombres[numEmpleados]);
+
+    cout << "Ingrese el tipo de empleado (1-Operario, 2-Tecnico, 3-Profesional): ";
+    cin >> tipos[numEmpleados];
+
+    cout << "Ingrese el salario por hora: ";
+    cin >> salariosPorHora[numEmpleados];
+
+    cout << "Ingrese la cantidad de horas trabajadas: ";
+    cin >> horasTrabajadas[numEmpleados];
+
+    calcularSalarios(numEmpleados);
+
+    numEmpleados++;
 }
 
 
