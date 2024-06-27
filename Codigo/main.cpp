0bfa458bc469da6b8cb47e563254033de02fff0a
chicho377 <morafabricio86@gmail.com>
2024-06-27 15:38:55 -0600

    Agregando funcion para mostrar menu

--- Codigo/main.cpp
+++ Codigo/main.cpp
@@ -4,8 +4,10 @@
 #include <cstdlib> // para usar system("CLS")
 using namespace std;
 
-const int MAX_EMPLEADOS = 100;
+// variables globales
+const int MAX_EMPLEADOS = 100; // constante para el tamano de los arrays
 
+// arrays
 string cedulas[MAX_EMPLEADOS];
 string nombres[MAX_EMPLEADOS];
 int tipos[MAX_EMPLEADOS];
@@ -19,11 +21,35 @@ double salariosNetos[MAX_EMPLEADOS];
 
 string tiposEmpleados[] = {"", "Operario", "Tecnico", "Profesional"};
 
+// variables contadores / calculos
 int numEmpleados = 0;
 int cantOperarios = 0, cantTecnicos = 0, cantProfesionales = 0;
 double acumuladoNetoOperarios = 0, acumuladoNetoTecnicos = 0, acumuladoNetoProfesionales = 0;
 
+// inicializando las funciones para poder agregarlas al main
+void mostrarMenu() {
+    cout << "Menu Principal:\n";
+    cout << "1. Agregar empleado\n";
+    cout << "2. Consultar empleado\n";
+    cout << "3. Modificar empleado\n";
+    cout << "4. Borrar empleado\n";
+    cout << "5. Mostrar estadisticas finales\n";
+    cout << "6. Salir\n";
+    cout << "Ingrese una opcion: ";
+}
+
 
 int main(int argc, char** argv) {
+	int opcion;
+	do{
+		system("CLS"); // limpiar pantalla
+		mostrarMenu();
+		cin >> opcion;
+		
+		system("CLS");
+	}while(opcion != 6);
+	
+	
+
 	return 0;
 }
